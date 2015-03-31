ce6bfd96bfc32be0469767e1275aba06e9231a8e
Phil Cohen <github@phlippers.net>
2015-03-31 13:50:17 -0700

    update `canon_algorithm` references:
    * only allow `canon_algorithm` method to take a string
    * rename variables to avoid circular references

--- lib/xml_security.rb
+++ lib/xml_security.rb
@@ -39,12 +39,8 @@ module XMLSecurity
     DSIG            = "http://www.w3.org/2000/09/xmldsig#"
 
     def canon_algorithm(element)
-      algorithm = element
-      if algorithm.is_a?(REXML::Element)
-        algorithm = element.attribute('Algorithm').value
-      end
-
-      case algorithm
+      case element
+        when "http://www.w3.org/2001/10/xml-exc-c14n#"         then Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
         when "http://www.w3.org/TR/2001/REC-xml-c14n-20010315" then Nokogiri::XML::XML_C14N_1_0
         when "http://www.w3.org/2006/12/xml-c14n11"            then Nokogiri::XML::XML_C14N_1_1
         else                                                        Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
@@ -244,38 +240,22 @@ module XMLSecurity
       )
       noko_sig_element = document.at_xpath('//ds:Signature', 'ds' => DSIG)
       noko_signed_info_element = noko_sig_element.at_xpath('./ds:SignedInfo', 'ds' => DSIG)
-      canon_algorithm = canon_algorithm REXML::XPath.first(
-        @sig_element,
-        '//ds:CanonicalizationMethod',
-        'ds' => DSIG
-      )
-      canon_string = noko_signed_info_element.canonicalize(canon_algorithm)
+      canon_algorithm_value = canon_algorithm(REXML::XPath.first(@sig_element, '//ds:CanonicalizationMethod', 'ds' => DSIG).attribute('Algorithm').value)
+      canon_string = noko_signed_info_element.canonicalize(canon_algorithm_value)
       noko_sig_element.remove
 
       # check digests
       REXML::XPath.each(@sig_element, "//ds:Reference", {"ds"=>DSIG}) do |ref|
-        uri = ref.attributes.get_attribute("URI").value
+        uri                           = ref.attributes.get_attribute("URI").value
 
-        hashed_element = document.at_xpath("//*[@ID=$uri]", nil, { 'uri' => uri[1..-1] })
-        canon_algorithm = canon_algorithm REXML::XPath.first(
-          ref,
-          '//ds:CanonicalizationMethod',
-          { "ds" => DSIG }
-        )
-        canon_hashed_element = hashed_element.canonicalize(canon_algorithm, inclusive_namespaces)
+        hashed_element                = document.at_xpath("//*[@ID='#{uri[1..-1]}']")
+        canon_algorithm_value         = canon_algorithm REXML::XPath.first(ref, '//ds:CanonicalizationMethod', 'ds' => DSIG).attribute('Algorithm').value
+        canon_hashed_element          = hashed_element.canonicalize(canon_algorithm_value, inclusive_namespaces)
 
-        digest_algorithm = algorithm(REXML::XPath.first(
-          ref,
-          "//ds:DigestMethod",
-          { "ds" => DSIG }
-        ))
-        hash = digest_algorithm.digest(canon_hashed_element)
-        encoded_digest_value = REXML::XPath.first(
-          ref,
-          "//ds:DigestValue",
-          { "ds" => DSIG }
-        ).text
-        digest_value = Base64.decode64(encoded_digest_value)
+        digest_algorithm              = algorithm(REXML::XPath.first(ref, "//ds:DigestMethod", 'ds' => DSIG))
+
+        hash                          = digest_algorithm.digest(canon_hashed_element)
+        digest_value                  = Base64.decode64(REXML::XPath.first(ref, "//ds:DigestValue", {"ds"=>DSIG}).text)
 
         unless digests_match?(hash, digest_value)
           @errors << "Digest mismatch"
